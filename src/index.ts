38fb41053751b68fba5872fa97237ade072a23be
gusberco <auguste.berco@gmail.com>
2021-06-08 11:20:57 +0200

    move package in its own repo

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// global window
+import { CountVars } from './index.d';
+import { useEffect, useState } from 'react';
+
+// TODO: Skip own views inspo: https://coding4gaiama.goatcounter.com/settings#tab-site-code
+// NOTE: OutboundLink use ref as replacement for GAs eventLabel?
+
+export const useGoatCounter = () => {
+  const [count, setCount] = useState({ count: (_: CountVars) => {} });
+
+  useEffect(() => {
+    const t = window.setInterval(() => {
+      if (window.goatcounter?.count) {
+        window.clearInterval(t);
+        setCount({ count: window.goatcounter.count });
+      }
+    }, 100);
+
+    return () => {
+      window.clearInterval(t);
+    };
+  }, []);
+
+  return count.count;
+};
